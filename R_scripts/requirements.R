ae21106804b6f550e20ef62395d37585b2f886d4
gconway012 <50531363+gconway012@users.noreply.github.com>
2020-11-13 17:06:47 -0500

    add DT package to requirements.R

--- R_scripts/requirements.R
+++ R_scripts/requirements.R
@@ -1,7 +1,7 @@
 ## load required packages ####
 
 # tidyverse package - for data read & manipulation
-tidyverse = if (!require("tidyverse")){
+if (!require("tidyverse")){
     install.packages("tidyverse", 
                      repos = "https://cran.rstudio.com/",
                      quiet = T)
@@ -9,7 +9,7 @@ tidyverse = if (!require("tidyverse")){
 }
             
 # kableExtra package - create html tables from dataframes
-kableExtra = if (!require("kableExtra")){
+if (!require("kableExtra")){
     install.packages("kableExtra", 
                      repos = "https://cran.rstudio.com/",
                      quiet = T)
@@ -17,7 +17,7 @@ kableExtra = if (!require("kableExtra")){
 }
 
 # RMySQL package - connecting to & query MySQL database
-RMySQL = if (!require("RMySQL")){
+if (!require("RMySQL")){
     install.packages("RMySQL", 
                      repos = "https://cran.rstudio.com/",
                      quiet = T)
@@ -25,9 +25,17 @@ RMySQL = if (!require("RMySQL")){
 }
 
 # plotly package - for making ggplots interactive
-plotly = if (!require("plotly")){
+if (!require("plotly")){
     install.packages("plotly", 
                      repos = "https://cran.rstudio.com/",
                      quiet = T)
     require("plotly")
 }
+
+## DT package - for making filterable/searchable HTML tables
+if(!require("DT")){
+    install.packages("DT",
+                     repos = "https://cran.rstudio.com/",
+                     quiet = T)
+    require("DT")
+} 
